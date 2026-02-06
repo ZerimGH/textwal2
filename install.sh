5cf994966208e4485848876fff098480dcc00071
lewis <lewdon0001@gmail.com>
2026-02-06 03:14:42 +0000

    Update paths

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ make all
 sudo install -Dm755 ./build/textwal2 /usr/bin/textwal2
 
 # Write default files
-mkdir -p ~/.config/textwal
+mkdir -p ~/.config/textwal2
 
-cat <<EOL > ~/.config/textwal/config.toml
+cat <<EOL > ~/.config/textwal2/config.toml
 [render]
 width = 1920
 height = 1080
@@ -27,13 +27,13 @@ opacity = 1.0
 command="echo 'Hello, world!'"
 
 [wallpaper]
-path="~/.config/textwal/output.png"
-set_command="~/.config/textwal/set-wallpaper.sh"
+path="~/.config/textwal2/output.png"
+set_command="~/.config/textwal2/set-wallpaper.sh"
 EOL
 
-cat <<EOL > ~/.config/textwal/set-wallpaper.sh
+cat <<EOL > ~/.config/textwal2/set-wallpaper.sh
 #!/usr/bin/env bash
 
-WALLPAPER_DIR=~/.config/textwal/output.png
-echo 'Please update ~/.config/textwal/set-wallpaper.sh to properly set your wallpaper.'
+WALLPAPER_DIR=~/.config/textwal2/output.png
+echo 'Please update ~/.config/textwal2/set-wallpaper.sh to properly set your wallpaper.'
 EOL
